f3bc0782155d66feef98a9736c53a9aebb8f7169
you <info@ez-design.net>
2017-01-15 13:36:56 +0900

    dockerfile修正

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,6 +9,7 @@ dotnet publish -o /RTFree/
 cd /src/RTFreeWeb/src/RTFreeWeb/
 dotnet restore
 dotnet publish -o /RTFreeWeb/
+mkdir /RTFreeWeb/wwwroot/records
 mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e 'create database rtfreeweb'
 mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD rtfreeweb < /src/RTFreeWeb/create.sql
 dotnet /RTFreeWeb/RTFreeWeb.dll
